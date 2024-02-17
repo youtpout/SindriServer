dcb90ec3aa09dbe9dd3ea09504e7df3a5ceb7c04
Youtpout <EddyBoughioul@hotmail.fr>
2024-02-17 11:18:40 +0100

    show swagger all times

--- SindriServer/Program.cs
+++ SindriServer/Program.cs
@@ -3,7 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(builder => {
+    options.AddDefaultPolicy(builder =>
+    {
         builder.AllowAnyOrigin();
         builder.AllowAnyMethod();
         builder.AllowAnyHeader();
@@ -18,11 +19,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
