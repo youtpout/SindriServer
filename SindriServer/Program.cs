af30b8f9f3d683e59576a9332b903c07f59d59c3
Youtpout <EddyBoughioul@hotmail.fr>
2024-02-17 11:18:16 +0100

    update cors

--- SindriServer/Program.cs
+++ SindriServer/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder => {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
